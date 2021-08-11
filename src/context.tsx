9a9f334664c3a4eb008d45e670a730ee88e89a44
Robertosnap <robertosnap@pm.me>
2021-08-11 19:21:17 +0200

    Added CAIP and wallet

--- src/context.tsx
+++ src/context.tsx
@@ -14,9 +14,9 @@ export type Dispatch<T = any> = React.Dispatch<React.SetStateAction<T>>;
 
 export interface IContext {
     loading: boolean;
-    // chains: string[];
-    // accounts: string[];
-    // wallet: Wallet | undefined;
+    chains: string[];
+    accounts: string[];
+    wallet: Wallet | undefined;
     // client: Client | undefined;
     // proposal: SessionTypes.Proposal | undefined;
     // setProposal: Dispatch<SessionTypes.Proposal | undefined>;
@@ -28,9 +28,9 @@ export interface IContext {
 
 export const INITIAL_CONTEXT: IContext = {
     loading: false,
-    // chains: [],
-    // accounts: [],
-    // wallet: undefined,
+    chains: [],
+    accounts: [],
+    wallet: undefined,
     // client: undefined,
     // proposal: undefined,
     // setProposal: () => {},
@@ -43,9 +43,9 @@ export const Context = createContext<IContext>(INITIAL_CONTEXT);
 
 export const ContextProvider = (props: any) => {
     const [loading, setLoading] = useState<boolean>(true);
-    // const [chains] = useState<string[]>(DEFAULT_TEST_CHAINS);
-    // const [accounts, setAccounts] = useState<string[]>([]);
-    // const [wallet, setWallet] = useState<Wallet | undefined>(undefined);
+    const [chains] = useState<string[]>(["eip155:421611"]);
+    const [accounts, setAccounts] = useState<string[]>([]);
+    const [wallet, setWallet] = useState<Wallet | undefined>(undefined);
     // const [client, setClient] = useState<Client | undefined>(undefined);
     // const [proposal, setProposal] = useState<SessionTypes.Proposal | undefined>(
     //     undefined,
@@ -54,35 +54,36 @@ export const ContextProvider = (props: any) => {
     //     SessionTypes.RequestEvent | undefined
     // >(undefined);
 
-    useEffect(
-        () => {
-            const initWallet = async () => {
-                console.log(`Starting Wallet...`);
-                const storage = new KeyValueStorage({
-                    asyncStorage: AsyncStorage as any,
-                });
-                const _wallet = await Wallet.init({
-                    chains: ["eip155:421611"],
-                    storage,
-                });
-                console.log(await _wallet.getAccounts());
-                setTimeout(() => {
-                    setLoading(false);
-                }, 500);
-            };
-            initWallet();
-        },
-        [
-            /* chains */
-        ],
-    );
+    useEffect(() => {
+        const initWallet = async () => {
+            console.log(`Starting Wallet...`);
+            const storage = new KeyValueStorage({
+                asyncStorage: AsyncStorage as any,
+            });
+            const _wallet = await Wallet.init({
+                chains,
+                storage,
+            });
+            const _accounts = await _wallet.getAccounts();
+            setAccounts(_accounts);
+            setWallet(_wallet);
+            console.log(_accounts);
+            // todo only test
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        };
+        initWallet();
+    }, [chains]);
+
+    useEffect(() => {}, []);
 
     // Make the context object:
     const context: IContext = {
         loading,
-        // chains,
-        // accounts,
-        // wallet,
+        chains,
+        accounts,
+        wallet,
         // client,
         // proposal,
         // setProposal,
